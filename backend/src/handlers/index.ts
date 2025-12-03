b9ca53a25dafde5220558356872b41d77e513e58
FelipeHerreraa <feli201412@gmail.com>
2025-12-03 05:26:13 -0600

    feat: add JWT generation utility, enhance CORS configuration, and implement login functionality with error handling

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -3,6 +3,7 @@ import { validationResult } from "express-validator"
 import slug from "slug"
 import User from "../models/User"
 import { checkPassword, hashPassword } from "../utils/auth"
+import { generateJWT } from "../utils/jwt"
 
 export const createAccount = async (req: Request, res: Response) => {
 
@@ -54,6 +55,6 @@ export const login = async (req: Request, res: Response) => {
         const error = new Error('El password es incorrecto')
         return res.status(401).json({ error: error.message })
     }
-
-    res.send('Login correcto')
+    const token = generateJWT({id: user._id})
+    res.send(token)
 }
